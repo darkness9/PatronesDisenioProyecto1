4e298a0260d8e993feb5414625e1b9a059c111a4
darkness9 <leongerard9@hotmail.com>
2015-02-20 18:28:20 -0600

    Creanlo el listanoticias.addMouseListener

--- src/View/ListaNoticiasJPanel.java
+++ src/View/ListaNoticiasJPanel.java
@@ -1,11 +1,15 @@
 package View;
 
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.SpringLayout;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  * @author Gerardo Hernández, Hugo Daniel Cabrera
@@ -46,6 +50,33 @@ public class ListaNoticiasJPanel extends javax.swing.JPanel{
         elscroll.setOpaque(false);
         elscroll.getViewport().setOpaque(false);     
          
+        listadenoticias.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
         //Se agregan los alementos al panel
         this.add(elscroll);
         if(controlfondo==true){
